20f38d7f5db672c0bb39372b063577b77fc7b552
Oleksandr Malakhov <71847145+hisio13@users.noreply.github.com>
2025-05-10 11:47:58 +0200

    update config

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import filtersReducer from './filtersSlice';
 const persistConfig = {
     key: 'contacts',
     storage,
-    whitelist: ['contacts'],
+    whitelist: ['items'],
   };
   
   const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
